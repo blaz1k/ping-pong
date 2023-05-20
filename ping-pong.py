4e578acad008c7697c3541664ffa1cf3ea777a13
блазик <132152761+blaz1k@users.noreply.github.com>
2023-05-20 18:38:38 +0300

    v2.0
    added ball remade rockets textures made an moving ball script

--- ping-pong.py
+++ ping-pong.py
@@ -1,11 +1,12 @@
 
 from pygame import *
 font.init()
+font1= font.SysFont("Arial", 72)
 font2= font.SysFont("Arial", 36)
 main_win = display.set_mode((700,500))
 display.set_caption('боевик епересете')
 clock= time.Clock()
-FPS= 60
+FPS= 120
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, x, y, speed, w, h):
@@ -18,25 +19,29 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         main_win.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player1(GameSprite):
-    def update(self):
+class Player(GameSprite):
+    def update0(self):
         keys_pressed= key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y<620:
+        if keys_pressed[K_s] and self.rect.y<400:
             self.rect.y+= self.speed
-        if keys_pressed[K_s] and self.rect.y>0:
+        if keys_pressed[K_w] and self.rect.y>0:
+            self.rect.y-= self.speed
+    def update1(self):
+        keys_pressed= key.get_pressed()
+        if keys_pressed[K_DOWN] and self.rect.y<400:
+            self.rect.y+= self.speed
+        if keys_pressed[K_UP] and self.rect.y>0:
             self.rect.y-= self.speed
 
-class Player2(GameSprite):
-    def update(self):
-        keys_pressed= key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y<600:
-            self.rect.y-= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y>0:
-            self.rect.y+= self.speed
+speed_x = 3
+speed_y =3
 
-
-left_pl= Player2("stick.png", 50, 50, 10 ,10, 100)
-background= transform.scale(image.load('bg.jpg'),(700,500))
+win_right_label= font1.render("победа правого",True, (0, 0 ,255))
+win_left_label= font1.render("победа левого", True, (255, 0 ,0))
+ball= GameSprite("snowball.png", 50, 50, 3, 50, 50)
+left_pl= Player("shield_r.png", 0, 50, 10 ,50, 100)
+right_pl= Player("shield.png", 650, 50, 10 ,50, 100)
+background= transform.scale(image.load('background.png'),(700,500))
 finish= True
 game = True
 while game!= False:
@@ -46,6 +51,23 @@ while game!= False:
     if finish == True:
         main_win.blit(background, (0, 0))
         left_pl.reset()
-        left_pl.update()
+        left_pl.update0()
+        ball.reset()
+        right_pl.reset()
+        right_pl.update1()
+        ball.rect.y += speed_y
+        ball.rect.x += speed_x
+        if ball.rect.y< 0 or ball.rect.y> 450:
+            speed_y *= -1
+        if sprite.collide_rect(ball, right_pl) or sprite.collide_rect(ball, left_pl):
+            speed_x *= -1
+        
+        if ball.rect.x <-50:
+            main_win.blit(win_right_label, (150, 250))
+            finish = False
+        if ball.rect.x >750:
+            main_win.blit(win_left_label, (150, 200))
+            finish = False
     display.update()
     clock.tick(FPS)
+    
